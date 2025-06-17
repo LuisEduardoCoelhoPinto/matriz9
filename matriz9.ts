76bfb23aeba8d20a0d375df8b6cab213a8b8a744
LuisEduardoCoelhoPinto <luis.coelho1501@gmail.com>
2025-06-17 13:49:24 -0300

    matriz9

--- matriz9.ts
+++ matriz9.ts
@@ -2,10 +2,8 @@ let matriz = [];
 let soma = 0;
 
 for (let i = 0; i < 6; i++) {
-    matriz[i] = [];
     for (let j = 0; j < 6; j++) {
         let input = prompt(`Elemento [${i}][${j}]:`);
-        matriz[i][j] = parseInt(input);
     }
 }
 
